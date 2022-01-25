b2cdbe1bbe18331441e56b006dcd9ab56e67b279
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-01-24 19:12:34 -0500

    completed tests for episode component

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,8 +4,44 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+const fakeEpisode = {
+    id: 123,
+  image: 'https://i.ibb.co/2FsfXqM/stranger-things.png',
+  name: 'The Monster',
+  season: 5,
+  number: 3,
+  summary: 'Here is a summary',
+  runtime: '56 min'
+}
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders without error", () => {
+    render(<Episode episode={fakeEpisode}/>);
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={fakeEpisode} />);
+
+    const summary = screen.getByText(/here is a summary/i);
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent(/here is a summary/i);
+});
+
+test("renders default image when image is not defined", ()=>{
+    const fakeEpisodeTwo = {
+        id: 123,
+  image: null,
+  name: 'Under the bed',
+  season: 4,
+  number: 6,
+  summary: 'Here is a summary',
+  runtime: '550 min'
+    }
+
+    render(<Episode episode={fakeEpisodeTwo}/>);
+
+    const altTag = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+
+    expect(altTag).toBeInTheDocument();
+});
